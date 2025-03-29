ae92af0c999ea729cbc2bee4b4662ead3755e793
RuokGamerz <79328354+HACKTEK90@users.noreply.github.com>
2025-03-29 22:04:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+(function() {
+    var parts = ["aHR0cHM6Ly9hYy1maW53aXNlLnZlcmNlbC5hcHAv"]; // Base64 encoded URL
+    var iframe = document.createElement("iframe");
+    iframe.src = atob(parts[0]); // Decode and set iframe src
+    iframe.setAttribute("scrolling", "yes");
+    iframe.setAttribute("loading", "lazy");
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.border = "none";
+    document.getElementById("iframeContainer").appendChild(iframe);
+})();
 
